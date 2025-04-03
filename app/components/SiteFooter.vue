0440d85691d1dbfeaa4f558c4cd1040adb12bdeb
qwq-YvYang <153091220+qwq-YvYang@users.noreply.github.com>
2025-04-03 21:37:50 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/qwq-YvYang",
+  home: "https://blog.yvyang.fun/",
+  email: "minecraftybc1@163.com",
 };
 </script>
 
